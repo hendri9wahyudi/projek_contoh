38f210a3de8693ed9dd1bdb4e568abb9c5c79d42
hendri9wahyudi <hendriwah07@gmail.com>
2025-03-13 09:48:17 +0700

    mengubah teks

--- lib/projek1.dart
+++ lib/projek1.dart
@@ -11,7 +11,7 @@ class HomePage extends StatelessWidget {
         backgroundColor: Colors.blue,
       ),
       body: const Center(
-        child: Text('Selamat datang disini :)'),
+        child: Text('Selamat Pagi'),
       ),
     );
   }
